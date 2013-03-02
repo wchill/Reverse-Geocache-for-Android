cc441dc62971de2d399cff2cae599cfab507b372
Eric Ahn <wchill1337@gmail.com>
2013-03-02 01:27:44 -0800

    Fixed dependency issues

--- Reverse Geocache/src/com/androchill/reversegeocache/IOIOFragmentActivity.java
+++ Reverse Geocache/src/com/androchill/reversegeocache/IOIOFragmentActivity.java
@@ -27,11 +27,12 @@
  * or implied.
  */
 
-package ioio.lib.util.android;
+package com.androchill.reversegeocache;
 
 import ioio.lib.impl.SocketIOIOConnection;
 import ioio.lib.util.IOIOLooper;
 import ioio.lib.util.IOIOLooperProvider;
+import ioio.lib.util.android.IOIOAndroidApplicationHelper;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
